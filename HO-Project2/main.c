069b38e5683a1bf71b46a9545a73b53ef714f3e9
JensNevens <jens.nevens@gmail.com>
2016-04-12 10:27:46 +0200

    Initial Commit

---
+++ HO-Project2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HO-Project2
+//
+//  Created by Jens Nevens on 12/04/16.
+//  Copyright © 2016 Jens Nevens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
